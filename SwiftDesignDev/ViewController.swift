8f63b7808508c80663cf384cc5768df9fe56facb
Chi Yeung <yeun0007@umn.edu>
2020-06-10 14:11:05 -0500

    new iOS project from XCode 11.1

---
+++ SwiftDesignDev/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftDesignDev
+//
+//  Created by yeuchi on 6/10/20.
+//  Copyright © 2020 yeuchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
