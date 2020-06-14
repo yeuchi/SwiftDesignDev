ccf7ef190c8b57ff85f4330e9456c540b309b4fc
Chi Yeung <yeun0007@umn.edu>
2020-06-13 21:04:04 -0500

    WIP : NSURLSession lesson
    1. created new controller, table cell, content in storyboard.
    2. ported above classes from course zip file, updated and fixed issues.

---
+++ NSUserDefault/NSUserDefault/FeedItem.swift
@@ -0,0 +1,14 @@
+//
+//  FeedItem.swift
+//  PhotoFeed
+//
+//  Created by Mike Spears on 2016-01-08.
+//  Copyright © 2016 YourOganisation. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem {
+    let title: String
+    let imageURL: NSURL
+}
